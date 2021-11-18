9732fb88c136d8f6adafa508aec619ae06aae6df
Darryl Tec <darryl.t@getjobber.com>
2021-11-18 09:59:51 -0700

    better test data

--- packages/components/src/WeekView/Test.tsx
+++ packages/components/src/WeekView/Test.tsx
@@ -9,17 +9,17 @@ export function Test() {
       data={[
         {
           user: { avatar: <Avatar initials="DT" />, name: "Darryl Tec" },
-          events: randomizedDates(),
+          events: randomizedEvents(),
         },
         {
           user: { avatar: <Avatar initials="CM" />, name: "Chris Murray" },
-          events: randomizedDates(),
+          events: randomizedEvents(),
         },
       ]}
     />
   );
 
-  function randomizedDates() {
+  function randomizedEvents() {
     const dates = Array.from(Array(10).keys()).map(i => {
       const startDate = new Date(
         +new Date() - Math.floor(Math.random() * 400000000 + i),
@@ -28,10 +28,16 @@ export function Test() {
         +startDate + Math.floor(Math.random() * 400000000 + i),
       );
 
+      const accent = ["#99c146", "#4b6A96", "#ef5733", "#ddc30f", "#556acb"];
+      const bgColors = ["#f0f6e3", "#e4e9ef", "#fde6e0", "#faf6db", "#c9cfee"];
+      const colorIndex = Math.floor(Math.random() * 4) + 0;
+
       return {
         content: "Event name",
         startAt: startDate,
         endAt: endDate,
+        accent: accent[colorIndex],
+        background: bgColors[colorIndex],
       };
     });
 
